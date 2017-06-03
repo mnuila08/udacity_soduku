248dbff4cffe25d146ce52d151fbcde4d4e6610f
mnuila08 <maria.nuila08@gmail.com>
2017-06-02 23:21:07 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,22 @@
 
-
-#Global variables:
+# Global variables:
 rows = 'ABCDEFGHI'
 cols = '123456789'
 def cross(A, B):
     "Cross product of elements in A and elements in B."
-    return [a+b for a in A for b in B]
+    return [ a +b for a in A for b in B]
 
-#Assign global values: (used in regular sodoku)
+
+# Assign global values: (used in regular sodoku)
 boxes = cross(rows, cols)
-unitlist = ([cross(rows, c) for c in cols] + [cross(r, cols) for r in rows] + [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123', '456','789')])
+unitlist = \
+([cross(rows, c) for c in cols] + [cross(r, cols) for r in rows] + [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs
+                                                                   in ('123', '456', '789')])
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
 
 assignments = []
+
 
 def assign_value(values, box, value):
     """
@@ -25,51 +28,64 @@ def assign_value(values, box, value):
         assignments.append(values.copy())
     return values
 
+
+def find_naked_twins_unit(values, unit):
+    naked_twins = []
+    # Find all instances of naked twins
+    for box in unit:
+        # if only two values in the box, continue:
+        if len(values[box]) == 2:
+            for peer in peers[box]:
+                if values[peer] == values[peer] and box != peer:  # check it's not the same box
+                    naked_twins.append([box, peer])
+    return naked_twins if len(naked_twins) > 0 else False
+
+
+def remove_naked_twins_unit(values, naked_twins):
+    for twins in naked_twins:
+        peers_tw1 = set(peers[twins[0]])
+        peers_tw2 = set(peers[twins[1]])
+        peers_12 = set(peers_tw1).intersection(peers_tw2)  # want the peers that exist in both (intersection)
+        for pbox in peers_12:
+            # check if pbox is not one of the twins and its values are more than 2:
+            if pbox != twins[0] and pbox != twins[1] and len(values[pbox]) > 2:
+                values = assign_value(values, pbox, values[pbox].replace(values[twins[0]][0], ''))
+                values = assign_value(values, pbox, values[pbox].replace(values[twins[0]][1], ''))
+    return values
+
+
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
         Args:
         values(dict): a dictionary of the form {'box_name': '123456789', ...}
-        
+
         Returns:
         the values dictionary with the naked twins eliminated from peers.
         """
-    
+
     naked_twins = []
-    # Find all instances of naked twins
-    for box in values:
-        #if only two values in the box, continue:
-        if len(values[box]) == 2:
-            for peer in peers[box]:
-                if values[peer] == values[peer] and box != peer: #check it's not the same box
-                    naked_twins.append([box, peer])
+    for unit in unitlist:
+        naked_twins = find_naked_twins_unit(values, unit)
+        if naked_twins and len(naked_twins) > 0:
+            for value in naked_twins:
+                values = remove_naked_twins_unit(value, naked_twins)
 
-# Eliminate the naked twins as possibilities for their units:
-for twins in naked_twins:
-    peers_tw1 = set(peers[twins[0]])
-        peers_tw2 = set(peers[twins[1]])
-        peers_12 = set(peers_tw1).intersection(peers_tw2) #want the peers that exist in both (intersection)
-        for pbox in peers_12:
-            #check if pbox is not one of the twins and its values are more than 2:
-            if pbox != twins[0] and pbox != twins[1] and len(values[pbox]) > 2:
-                values = assign_value(values, pbox, values[pbox].replace(values[twins[0]][0], ''))
-                values = assign_value(values, pbox, values[pbox].replace(values[twins[0]][1], ''))
+    return values
 
-return values
+
+# ----------------------------------------------------------------------------------------------------------------#
+# Problem 2:
 
 
 
-#----------------------------------------------------------------------------------------------------------------#
-#Problem 2:
-
-
-
-#Create our diagonals:
-diagonal1 = [rows[i] + cols[i] for i in range(len(rows))] #A1, B2, ...I9
+# Create our diagonals:
+diagonal1 = [rows[i] + cols[i] for i in range(len(rows))]  # A1, B2, ...I9
 reverse_cols = cols[::-1]
-diagonal2 = [rows[i] + reverse_cols[i] for i in range(len(rows))] #A9, B8, ..., I1
+diagonal2 = [rows[i] + reverse_cols[i] for i in range(len(rows))]  # A9, B8, ..., I1
 diag_unitlist = [diagonal1 + diagonal2] + unitlist
-diag_units = dict((s, [d for d in diag_unitlist if s in d]) for s in boxes) #create our dictionary of all units (including diagonal)
-diag_peers = dict((s , set(sum(diag_units[s], []))-set([s])) for s in boxes) #create our peers with diagonal entries
+diag_units = [[r+c for r,c in zip(rows, cols)], [r+c for r,c in zip(rows, cols[::-1])]]  # create our dictionary of all units (including diagonal)
+diag_peers = dict((s, set(sum(diag_units[s], [])) - set([s])) for s in boxes)  # create our peers with diagonal entries
+
 
 def grid_values(grid):
     """
@@ -90,6 +106,7 @@ def grid_values(grid):
             values.append(c)
     assert len(values) == 81
     return dict(zip(boxes, values))
+
 
 def display(values):
     """
@@ -115,9 +132,9 @@ def eliminate(values):
     for b in solved:
         d = values[b]
         for peer in diag_peers[b]:
-            values[peer] = values[peer].replace(d,'')
-
-return values
+            values[peer] = values[peer].replace(d, '')
+            
+    return values
 
 
 def only_choice(values):
@@ -130,6 +147,7 @@ def only_choice(values):
             dplaces = [box for box in unit if digit in values[box]]
             if len(dplaces) == 1:
                 values[dplaces[0]] = digit
+    return values
 
 
 def reduce_puzzle(values):
@@ -139,13 +157,15 @@ def reduce_puzzle(values):
         """
     stalled = False
     while not stalled:
-        #how many were solved before the reduction:
+        # how many were solved before the reduction:
         solved_before = len([box for box in values.keys() if len(values[box]) == 1])
-        #solve using eliminate strategy
+        # solve using eliminate strategy
         values = eliminate(values)
-        #solve using only_choice strategy
+        # solve using only_choice strategy
         values = only_choice(values)
-        #find out how many are solved now:
+        #reduce using naked_twins
+        values = naked_twins(values)
+        # find out how many are solved now:
         solved_after = len([box for box in values.keys() if len(values[box]) == 1])
         # If no new values were added, stop the loop.
         stalled = solved_before == solved_after
@@ -153,16 +173,17 @@ def reduce_puzzle(values):
         if len([box for box in values.keys() if len(values[box]) == 0]):
             return False
 
-return values
+    return values
+
 
 def search(values):
     "Using depth-first search and propagation, try all possible values."
     # First, reduce the puzzle using the previous function
     values = reduce_puzzle(values)
     if values == False:
-        return False #Failed earlier
+        return False  # Failed earlier
     if all(len(values[s] == 1 for s in boxes)):
-        return values #Solved
+        return values  # Solved
     # Choose one of the unfilled squares with the fewest possibilities
     n, s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
     # Now use recurrence to solve each one of the resulting sudokus, and
@@ -192,6 +213,7 @@ if __name__ == '__main__':
 
     try:
         from visualize import visualize_assignments
+
         visualize_assignments(assignments)
 
     except SystemExit:
